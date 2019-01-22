97aa9024bac97f5edc0db915fb4fdddd32edd011
Jacek Leszczyński <jacek.leszczynski@sendit.pl>
2019-01-22 22:07:04 +0100

    test xhr

--- getGlsTrackingInfo/index.js
+++ getGlsTrackingInfo/index.js
@@ -1,3 +1,5 @@
+const request = require('xhr-request');
+
 /**
  * Responds to any HTTP request.
  *
@@ -5,6 +7,13 @@
  * @param {!express:Response} res HTTP response context.
  */
 exports.getGlsTrackingInfo = (req, res) => {
+    const address = `https://gls-group.eu/app/service/open/rest/PL/pl/rstt001?match=${req.query.tracking}`;
     let message = req.query.message || req.body.message || 'Hello World!';
+    request('address', {
+        json: true
+    }, function (err, data) {
+        if (err) throw err;
+        console.log(data);
+    });
     res.status(200).send(message);
 };
