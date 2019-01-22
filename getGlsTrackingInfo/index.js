d34800e4b0b48805dc09ee7a41bd5ab268d6b65d
Jacek Leszczyński <jacek.leszczynski@sendit.pl>
2019-01-22 23:54:33 +0100

    404 i 400

--- getGlsTrackingInfo/index.js
+++ getGlsTrackingInfo/index.js
@@ -8,32 +8,38 @@ const request = require('xhr-request');
  */
 exports.getGlsTrackingInfo = (req, res) => {
     const address = `https://gls-group.eu/app/service/open/rest/PL/pl/rstt001?match=${req.query.tracking}`;
+    if(!req.query.tracking){
+        res.status(400).send({error: "tracking number required!"})
+    }
     request(address, {
         json: true
     }, function (err, data) {
         if (err) {
-            console.error(err);
             res.status(500).send({error: err})
         }
-        console.log(data);
-        const history = data.tuStatus[0].history;
-        let statuses = [];
-        for (let status of history) {
-            statuses.push({
-                status: status.evtDscr,
-                status_description: status.evtDscr,
-                datetime: status.date + 'T' + status.time,
-                place: status.address.city
-            })
-        }
+        if(data.tuStatus){
+            const history = data.tuStatus[0].history;
+            let statuses = [];
+            for (let status of history) {
+                statuses.push({
+                    status: status.evtDscr,
+                    status_description: status.evtDscr,
+                    datetime: status.date + 'T' + status.time,
+                    place: status.address.city
+                })
+            }
 
-        res.status(200).send(
-            {
-                tracking_number: req.query.tracking,
-                status: history[0].evtDscr,
-                status_description: history[0].evtDscr,
-                updated_at: history[0].date + 'T' + history[0].time,
-                tracking_details: statuses
-            });
+            res.status(200).send(
+                {
+                    tracking_number: req.query.tracking,
+                    status: history[0].evtDscr,
+                    status_description: history[0].evtDscr,
+                    updated_at: history[0].date + 'T' + history[0].time,
+                    tracking_details: statuses
+                });
+        }
+        else{
+            res.status(404).send({error: `Order with tracking number ${req.query.tracking} not found`})
+        }
     });
 };
